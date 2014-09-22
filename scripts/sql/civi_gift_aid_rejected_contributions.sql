e728d2867f81297344248835fa9ee08f9e24d873
solomon <solomon@vedaconsulting.co.uk>
2014-09-22 18:45:11 +0100

    join giftaid and custom search

--- scripts/sql/civi_gift_aid_rejected_contributions.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS `civicrm_gift_aid_rejected_contributions` (
-`id`                          int(10) unsigned NOT NULL auto_increment,
-`batch_id`                    int(10) unsigned NOT NULL,
-`contribution_id`             int(10) unsigned NOT NULL,
-`created_date`                timestamp        DEFAULT CURRENT_TIMESTAMP,
-`rejection_reason`            varchar(255)      NOT NULL,
-PRIMARY KEY  (`id`),
-KEY `git_aid_rejections_contribution_id` (`contribution_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;
